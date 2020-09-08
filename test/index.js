821dd6b0e5138b8a6302a65bc9c07639af99e0bd
hhh <1340545944@qq.com>
2020-09-08 20:50:21 +0800

    improve tests

--- test/index.js
+++ test/index.js
@@ -4,7 +4,9 @@ const T = require('3h-test');
 T.test(null, {
 
     range(context) {
+
         const { range } = Iter;
+
         context.assertShallowEqual(
             [...range(3)],
             [0, 1, 2]
@@ -33,18 +35,56 @@ T.test(null, {
             [...range(0, .5, .2)],
             [0, .2, .4]
         );
+
+        const iterator = range(Infinity)[Symbol.iterator]();
+        context.assertStrictEqual(
+            iterator.next().value,
+            0
+        );
+        context.assertStrictEqual(
+            iterator.next().value,
+            1
+        );
+        context.assertStrictEqual(
+            iterator.next().value,
+            2
+        );
+        context.assertStrictEqual(
+            Boolean(iterator.next().done),
+            false
+        );
+
         context.expectThrow(
-            () => [...range(0, 5, -1)],
-            RangeError
+            range,
+            RangeError,
+            [Infinity, 5]
         );
         context.expectThrow(
-            () => [...range(5, 0, 1)],
-            RangeError
+            range,
+            RangeError,
+            [0, 5, -1]
         );
         context.expectThrow(
-            () => [...range(0, 5, 0)],
-            RangeError
+            range,
+            RangeError,
+            [0, Infinity, -1]
         );
+        context.expectThrow(
+            range,
+            RangeError,
+            [0, -Infinity, 1]
+        );
+        context.expectThrow(
+            range,
+            RangeError,
+            [5, 0, 1]
+        );
+        context.expectThrow(
+            range,
+            RangeError,
+            [0, 5, 0]
+        );
+
     },
 
     chain(context) {
