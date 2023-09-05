a4dc2ae5e3d53b7c66b4acdb0f450689116246e8
hhh <1340545944@qq.com>
2023-09-05 11:31:50 +0800

    add next arg support to iter2fn

--- src/iter2fn.ts
+++ src/iter2fn.ts
@@ -1,13 +1,15 @@
-// TODO: accept arguments for next()
 /**
  * Create a function that returns value from the given iterable object
  * each time the function is invoked; when iteration is done, it returns
  * specific stop value instead.
  */
-export const iter2fn = <T, U>(iterable: Iterable<T>, stopValue: U): (() => (T | U)) => {
-    const iterator = iterable[Symbol.iterator]();
-    return () => {
-        const result = iterator.next();
+export const iter2fn = <T, U, TNext = unknown>(
+    iterable: Iterable<T>,
+    stopValue: U,
+): ((...args: ([] | [TNext])) => (T | U)) => {
+    const iterator = iterable[Symbol.iterator]() as Iterator<T, undefined, TNext>;
+    return (...args) => {
+        const result = iterator.next(...args);
         return result.done ? stopValue : result.value;
     };
 };
--- test/iter2fn.cjs
+++ test/iter2fn.cjs
@@ -5,11 +5,28 @@ const Iter = require("..");
  * @param {import('3h-test').TestContext} context
  */
 module.exports = (context) => {
+
     const { iter2fn } = Iter;
-    const fn = iter2fn([0, 1, 2], null);
-    context.assertStrictEqual(fn(), 0);
-    context.assertStrictEqual(fn(), 1);
-    context.assertStrictEqual(fn(), 2);
-    context.assertStrictEqual(fn(), null);
-    context.assertStrictEqual(fn(), null);
+
+    const fn1 = iter2fn([0, 1, 2], null);
+    context.assertStrictEqual(fn1(), 0);
+    context.assertStrictEqual(fn1(), 1);
+    context.assertStrictEqual(fn1(), 2);
+    context.assertStrictEqual(fn1(), null);
+    context.assertStrictEqual(fn1(), null);
+
+    function* generator() {
+        let value = yield 0;
+        while (value < 5) {
+            value = yield (value + 1);
+        }
+    }
+
+    const fn2 = iter2fn(generator(), -1);
+    context.assertStrictEqual(fn2(8), 0);
+    context.assertStrictEqual(fn2(1), 2);
+    context.assertStrictEqual(fn2(3), 4);
+    context.assertStrictEqual(fn2(), -1);
+    context.assertStrictEqual(fn2(), -1);
+
 };
