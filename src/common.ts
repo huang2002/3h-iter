b35bfab45d6e0575d15a6b2017ef1ebdb603854c
hhh <1340545944@qq.com>
2023-09-05 13:40:13 +0800

    add docs to common.ts

--- src/common.ts
+++ src/common.ts
@@ -1,7 +1,13 @@
-export type ToIterables<T extends readonly unknown[]> = {
+/**
+ * Map the given type to contain iterables.
+ */
+export type ToIterables<T extends {}> = {
     [K in keyof T]: Iterable<T[K]>;
 };
-
-export type MixinMap<T extends readonly unknown[], U> = {
+/** dts2md break */
+/**
+ * Mix type `U` into the values of type `T`.
+ */
+export type MixinMap<T extends {}, U> = {
     [K in keyof T]: T[K] | U;
 };
