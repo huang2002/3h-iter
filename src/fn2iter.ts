d3679f4a8240e5a36daf67a68068ab01ae31cd19
hhh <1340545944@qq.com>
2023-09-05 11:41:09 +0800

    add next arg support to fn2iter

--- src/fn2iter.ts
+++ src/fn2iter.ts
@@ -3,17 +3,20 @@
  * the given function; iteration is done when the function
  * returns specific stop value.
  */
-export const fn2iter = <T, U>(fn: () => T | U, stopValue: U): Iterable<T> => ({
-    [Symbol.iterator](): Iterator<T, undefined, undefined> {
+export const fn2iter = <T, U, TNext>(
+    fn: ((...args: ([] | [TNext])) => (T | U)),
+    stopValue: U,
+): Iterable<T> => ({
+    [Symbol.iterator](): Iterator<T, undefined, any> {
         return {
-            next() {
-                const value = fn();
+            next(...args: ([] | [TNext])) {
+                const value = fn(...args);
                 return (
                     (value === stopValue)
                         ? { done: true, value: undefined } as const
                         : { value: value as T }
                 );
             }
-        };
+        } satisfies Iterator<T, undefined, TNext>;
     }
 });
--- test/fn2iter.cjs
+++ test/fn2iter.cjs
@@ -8,18 +8,48 @@ module.exports = (context) => {
 
     const { fn2iter } = Iter;
 
-    const stop = 3;
-    let i = 0;
-
-    const fn = () => (
-        (i < stop) ? i++ : null
+    const fn1 = (() => {
+        const stop = 3;
+        let i = 0;
+        return () => (
+            (i < stop) ? i++ : null
+        );
+    })();
+    const iterable1 = fn2iter(fn1, null);
+    context.assertShallowEqual(
+        [...iterable1],
+        [0, 1, 2]
     );
 
-    const iter = fn2iter(fn, null);
-
+    /**
+     * @param {[] | [number]} args
+     */
+    const fn2 = (...args) => (
+        args.length ? args[0] : 0
+    );
+    const iterable2 = fn2iter(fn2, -1);
+    const iterator2 = /** @type {Iterator<number, undefined, number>} */(
+        iterable2[Symbol.iterator]()
+    );
     context.assertShallowEqual(
-        [...iter],
-        [0, 1, 2]
+        iterator2.next(),
+        { value: 0 },
+    );
+    context.assertShallowEqual(
+        iterator2.next(),
+        { value: 0 },
+    );
+    context.assertShallowEqual(
+        iterator2.next(2),
+        { value: 2 },
+    );
+    context.assertShallowEqual(
+        iterator2.next(8),
+        { value: 8 },
+    );
+    context.assertShallowEqual(
+        iterator2.next(-1),
+        { done: true, value: undefined },
     );
 
 };
