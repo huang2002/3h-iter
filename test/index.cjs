f2a6864d35eb855da9efcea44ce8182f22827881
hhh <1340545944@qq.com>
2023-09-05 12:49:54 +0800

    improve test script

--- test/index.cjs
+++ test/index.cjs
@@ -1,16 +1,5 @@
 // @ts-check
 const { test } = require('3h-test');
-const rangeTest = require('./range.cjs');
-const chainTest = require('./chain.cjs');
-const compressTest = require('./compress.cjs');
-const zipTest = require('./zip.cjs');
-const zipLongestTest = require('./zipLongest.cjs');
-const filterTest = require('./filter.cjs');
-const mapTest = require('./map.cjs');
-const sliceTest = require('./slice.cjs');
-const iter2fnTest = require('./iter2fn.cjs');
-const fn2iterTest = require('./fn2iter.cjs');
-const repeatTest = require('./repeat.cjs');
 
 test({
     include: (
@@ -19,15 +8,15 @@ test({
             : undefined
     ),
 }, {
-    range: rangeTest,
-    chain: chainTest,
-    compress: compressTest,
-    zip: zipTest,
-    zipLongest: zipLongestTest,
-    filter: filterTest,
-    map: mapTest,
-    slice: sliceTest,
-    iter2fn: iter2fnTest,
-    fn2iter: fn2iterTest,
-    repeat: repeatTest,
+    range: require('./range.cjs'),
+    chain: require('./chain.cjs'),
+    compress: require('./compress.cjs'),
+    zip: require('./zip.cjs'),
+    zipLongest: require('./zipLongest.cjs'),
+    filter: require('./filter.cjs'),
+    map: require('./map.cjs'),
+    slice: require('./slice.cjs'),
+    iter2fn: require('./iter2fn.cjs'),
+    fn2iter: require('./fn2iter.cjs'),
+    repeat: require('./repeat.cjs'),
 });
